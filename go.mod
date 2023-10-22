ce8e0ae09745e3a46a013bc37d8a9c9fb1ea73ed
Henry <henryjrose2@gmail.com>
2023-10-22 15:24:15 -0400

    Loggly monitoring, DynamoDB writing working

--- go.mod
+++ go.mod
@@ -2,9 +2,15 @@ module main.go
 
 go 1.21.1
 
-require github.com/PuerkitoBio/goquery v1.8.1
+require (
+	github.com/PuerkitoBio/goquery v1.8.1
+	github.com/aws/aws-sdk-go v1.46.1
+	github.com/jamespearly/loggly v0.0.0-20220929110912-1d839b0715fa
+)
 
 require (
+	github.com/TwiN/go-color v1.4.1 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
-	golang.org/x/net v0.7.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	golang.org/x/net v0.17.0 // indirect
 )
--- main.go
+++ main.go
@@ -8,19 +8,27 @@ import (
 	"github.com/PuerkitoBio/goquery"
 	"strconv"
 	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+    "github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/jamespearly/loggly"
 )
 
 type Datum struct {
 	Ticker string
-	DateTime time.Time
+	DateTime int64
 	Price float32
 }
 
 func main() {
 	var i int
-	stocks := [2]string{"AAPL", "SPY"}
 
-	flag.IntVar(&i, "i", 5, "Time Interval (5 for 5 seconds)")
+	client := loggly.New("Web Scraper")
+	
+	stocks := [4]string{"AAPL", "SPY", "AMZN", "TSLA"}
+
+	flag.IntVar(&i, "i", 60, "Time Interval (5 for 5 seconds)")
 	flag.Parse()
 
 	ticker := time.NewTicker(time.Duration(i) * time.Second)
@@ -32,16 +40,16 @@ func main() {
 				for _, stock := range stocks{
 					
 			
-					p := getStockPrice(stock)
-					s := fmt.Sprintf("%f", p)
-			
-					log.Println(s)
+					p := getStockPrice(stock)			
+					
 					var datum Datum
-					datum.DateTime = time.Now()
+					datum.DateTime = time.Now().Unix()
 					datum.Price = p
 					datum.Ticker = stock
-
-					
+					dynamoDBWrite(datum)
+					err := client.EchoSend("info", "Wrote Data for " + datum.Ticker)
+ 					if(err != nil){fmt.Println("err:", err)}
+	
 				}
 			}
 			
@@ -49,6 +57,7 @@ func main() {
 }
 
 func getStockPrice(Ticker string)(float32) {
+	client := loggly.New("Web Scraper")
 	resp, err := http.Get("https://finance.yahoo.com/quote/" + Ticker)
 	if(err != nil){
 		log.Println(err)
@@ -65,6 +74,7 @@ func getStockPrice(Ticker string)(float32) {
 
 	if marketPrice == "" {
 		log.Fatalf("Can't Find Price...")
+		client.Send("error", "Can't find price")
 	}
 
 	//I love this format for if statements, it's essentially a try...catch
@@ -72,4 +82,30 @@ func getStockPrice(Ticker string)(float32) {
 		return float32(float)
 	}
 	return float32(0)
+}
+func dynamoDBWrite(datum Datum){
+	client := loggly.New("Web Scraper")
+	// Initialize a session that the SDK will use to load
+	// credentials from the shared credentials file ~/.aws/credentials
+	// and region from the shared configuration file ~/.aws/config.
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	svc := dynamodb.New(sess)
+	av, err := dynamodbattribute.MarshalMap(datum)
+	if err != nil {
+		log.Fatalf("Error formatting data: %v", err)
+		client.Send("error", "Error formatting data")
+	}
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String("hrose3"),
+	}
+
+	_, err = svc.PutItem(input)
+	if err != nil {
+		log.Fatalf("Error calling PutItem: %v", err)
+		client.Send("error", "Error calling PutItem")
+	}
+
 }
